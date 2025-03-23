def32d4d98e5b21578d1b9f7e4ee0d248ab54c1d
rohitshirsat02 <127138343+rohitshirsat02@users.noreply.github.com>
2025-03-23 19:20:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //complete this code
-class Person {}
+class Person {
+	Constructor(name ,age){
+		this._name = name;
+		this._age = age;
+	}
+	get name(){
+		return this._name;
+	}
+	get age(){
+		return this.age;
+	}
+	set name(newName){
+		this._name = newName;
+	}
+	set age(newAge){
+		this._age = newAge;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teacher(){
+		console.log(`${this.name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
